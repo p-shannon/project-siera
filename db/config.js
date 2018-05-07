32864b6026ca581abef5e61f3b35a515da9964a1
Patrick Shannon <pshannon.dev@gmail.com>
2018-05-06 20:08:18 -0400

    Normalizes the mongo object property names.

--- db/config.js
+++ db/config.js
@@ -3,9 +3,9 @@
 const Mongo = {};
 
 //Import the mongo client
-Mongo.Client = require('mongodb').MongoClient;
+Mongo.client = require('mongodb').MongoClient;
 //And the object id bit
-Mongo.ObjectID = require('mongodb').ObjectID;
+Mongo.objectId = require('mongodb').ObjectID;
 //And specify the URl
 //TODO: We want to provide an option to set this from a dotenv file
 Mongo.url = 'mongodb://localhost:27017/psg';
