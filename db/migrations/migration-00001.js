4700fc2a4a2ac775ce47291acb865838b9dfd208
Patrick Shannon <pshannon.dev@gmail.com>
2018-05-09 17:52:38 -0400

    enables top-level validation for mobs

--- db/migrations/migration-00001.js
+++ db/migrations/migration-00001.js
@@ -1,4 +1,3 @@
-//This file is run from mongodb shell.
 connection = new Mongo();
 db = connection.getDB('psg');
 db.createCollection('mobs', {
@@ -17,90 +16,20 @@ db.createCollection('mobs', {
 				},
 				attribute: {
 					bsonType: "object",
-					required: [ "health", "living", "strength", "agility", "intelligence", "mana", "stamina", "max" ],
-					properties: {
-						health: {
-							bsonType: "int",
-							description: "The number of health points the character has. <INTEGER> [REQUIRED]",
-						},
-						living: {
-							bsonType: "bool",
-							description: "If the character is alive and playable. <BOOLEAN> [REQUIRED]"
-						},
-						strength: {
-							bsonType: "int",
-							minimum: 0,
-							maximum: 100,
-							description: "The number representing how brawny the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-						},
-						agility: {
-							bsonType: "int",
-							minimum: 0,
-							maximum: 100,
-							description: "The number representing how fast the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-						},
-						intelligence: {
-							bsonType: "int",
-							minimum: 0,
-							maximum: 100,
-							description: "The number representing how smart the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-						},
-						mana: {
-							bsonType: "int",
-							description: "The number of magic points the character has. <INTEGER> [REQUIRED]",
-						},
-						stamina: {
-							bsonType: "int",
-							description: "The number of energy points the character has. <INTEGER> [REQUIRED]",
-						},
-						max: {
-							bsonType: "object",
-							required: [ "health", "strength", "agility", "intelligence", "mana", "stamina" ],
-							properties: {
-								health: {
-									bsonType: "int",
-									description: "The maximum number of health points the character has. <INTEGER> [REQUIRED]",
-								},
-								strength: {
-									bsonType: "int",
-									minimum: 0,
-									maximum: 100,
-									description: "The maximum number representing how brawny the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-								},
-								agility: {
-									bsonType: "int",
-									minimum: 0,
-									maximum: 100,
-									description: "The maximum number representing how fast the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-								},
-								intelligence: {
-									bsonType: "int",
-									minimum: 0,
-									maximum: 100,
-									description: "The maximum number representing how smart the character is. <INTEGER> (0 > n > 101) [REQUIRED]"
-								},
-								mana: {
-									bsonType: "int",
-									description: "The maximum number of magic points the character has. <INTEGER> [REQUIRED]",
-								},
-								stamina: {
-									bsonType: "int",
-									description: "The maximum number of energy points the character has. <INTEGER> [REQUIRED]",
-								}
-							}
-						}
-					},
+					description: "The object containing various values representing the character's abilites. <OBJECT> [REQUIRED]"
+				},
 				flavor: {
 					bsonType: "object",
-					description: "Contains properties that describe various aspect of the character's appearance and demeanor. <OBJECT> [REQUIRED]"
+					description: "The object containing various properties describing the character's demeanor and visage. <OBJECT> [REQUIRED]"
 				},
 				action: {
 					bsonType: "object",
-					description: "Contains properties that indicate actions this character is capable of. <OBJECT> [REQUIRED]"
-				},
-			additionalProperties: false
+					description: "The object containing properties indicating the various actions they may perform. <OBJECT> [REQUIRED]"
+				}
 			}
-		}
+		},
+		validationLevel: "strict",
+		additionalProperties: false
 	}
-}
 });
+
