6869917556124349beb5f3eb536a5f58be554ec1
Patrick Shannon <pshannon.dev@gmail.com>
2018-05-09 18:14:34 -0400

    CORRECTLY enables top-level validation for mob objects coming into the database.

--- db/migrations/migration-00001.js
+++ db/migrations/migration-00001.js
@@ -1,11 +1,16 @@
 connection = new Mongo();
 db = connection.getDB('psg');
 db.createCollection('mobs', {
+	validationLevel: "strict",
 	validator: {
 		$jsonSchema: {
 			bsonType: "object",
 			required: [ "name", "species", "attribute", "flavor", "action" ],
+			additionalProperties: false,
 			properties: {
+				_id: {
+					bsonType: "objectId"
+				},
 				name: {
 					bsonType: "string",
 					description: "The name of the character. <STRING> [REQUIRED]"
@@ -16,20 +21,33 @@ db.createCollection('mobs', {
 				},
 				attribute: {
 					bsonType: "object",
-					description: "The object containing various values representing the character's abilites. <OBJECT> [REQUIRED]"
+					description: "The object containing various values representing the character's abilites. <OBJECT> [REQUIRED]",
+					properties: {
+						test: {
+							bsonType: "bool"
+						}
+					}
 				},
 				flavor: {
 					bsonType: "object",
-					description: "The object containing various properties describing the character's demeanor and visage. <OBJECT> [REQUIRED]"
+					description: "The object containing various properties describing the character's demeanor and visage. <OBJECT> [REQUIRED]",
+					properties: {
+						test: {
+							bsonType: "bool"
+						}
+					}
 				},
 				action: {
 					bsonType: "object",
-					description: "The object containing properties indicating the various actions they may perform. <OBJECT> [REQUIRED]"
+					description: "The object containing properties indicating the various actions they may perform. <OBJECT> [REQUIRED]",
+					properties: {
+						test: {
+							bsonType: "bool"
+						}
+					}
 				}
 			}
 		},
-		validationLevel: "strict",
-		additionalProperties: false
 	}
 });
 
