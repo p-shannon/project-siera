4de2c7f3c3b034f8226743bfb6d990f239c65bf2
Patrick Shannon <pshannon.dev@gmail.com>
2018-05-24 23:30:49 -0400

    Adds a 'logs' collection to the database.

---
+++ db/migrations/migration-00002.js
@@ -0,0 +1,26 @@
+db = new Mongo().getDB('psg');
+db.createCollection('logs', {
+	validationLevel: "strict",
+	validator: {
+		$jsonSchema: {
+			bsonType: "object",
+			required: ["timestamp", "type", "room", "content"],
+			additionalProperties: false,
+			properties: {
+				_id: {
+					bsonType: "objectId"
+				},
+				//Going to skimp out on the descriptions for now, they have no purpose.
+				timestamp: {
+					bsonType: "int"
+				},
+				room: {
+					bsonType: "string"
+				},
+				content: {
+					bsonType: "text"
+				}
+			}
+		}
+	}
+});
