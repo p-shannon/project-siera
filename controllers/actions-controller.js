ac805680a2f12e8c8c77fcb917dec9d7bcc8cecf
Patrick Shannon <pshannon.dev@gmail.com>
2018-05-26 00:03:39 -0400

    adds an action controller for managing actions taken, offloading the job from the log controller.

---
+++ controllers/actions-controller.js
@@ -0,0 +1,45 @@
+////Grab the mob model
+const Mob = require('../models/mob');
+////And the controller
+const mobsController = require('./mobs-controller');
+////And the log model
+const Log = require('../models/log');
+////Create the actions controller object
+const actionsController = {};
+
+////Give it logic
+//attacking
+
+actionsController.attack = function(req, res){
+	Promise.all([Mob.findById(req.params.attacker),Mob.findById(req.params.defender)])
+	.then(mobs => {
+		let remainingHealth = mobs[1].attribute.health - mobs[0].attribute.strength;
+		return Mob.update(req.params.defender,"attribute.health",remainingHealth)
+		.then(newMob => {
+			let newLog = {
+				content: `${mobs[0].name} attacks ${mobs[1].name} for ${mobs[0].attribute.strength} damage. ** ${mobs[0]._id} =${mobs[0].attribute.strength}=> ${mobs[1]._id} **`,
+				timestamp: Date.now(),
+				type: "action",
+				//TODO: update this to include involved room
+				room: null
+			}
+			return Log.create(newLog)
+			.then(logResponse => {
+				res.status(200)
+				.json({
+					message: "Attack completed successfully!",
+					log: logResponse,
+					attacker: mobs[0],
+					defender: newMob.value
+				})
+			})
+		})
+	})
+	.catch(err => {
+		console.log(err)
+		res.status(500).json({err})
+	})
+}
+
+////Export it.
+module.exports = actionsController;
