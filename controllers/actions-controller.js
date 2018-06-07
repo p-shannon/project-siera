911f15a1ff002a7914d69ab9f92c257c832b9160
Patrick Shannon <pshannon.dev@gmail.com>
2018-06-07 16:17:33 -0400

    Refactors the attack method on the action controller to be a little less rediculous and use the new Mob.takeDamage method.

--- controllers/actions-controller.js
+++ controllers/actions-controller.js
@@ -10,17 +10,47 @@ const actionsController = {};
 ////Give it logic
 //attacking
 
+//actionsController.attack = function(req, res){
+//	Promise.all([Mob.findById(req.params.attacker),Mob.findById(req.params.defender)])
+//	.then(mobs => {
+//		let remainingHealth = mobs[1].attribute.health - mobs[0].attribute.strength;
+//		return Mob.update(req.params.defender,"attribute.health",remainingHealth)
+//		.then(newMob => {
+//			let newLog = {
+//				content: `${mobs[0].name} attacks ${mobs[1].name} for ${mobs[0].attribute.strength} damage. ** ${mobs[0]._id} =${mobs[0].attribute.strength}=> ${mobs[1]._id} **`,
+//				timestamp: Date.now(),
+//				type: "action",
+//				//TODO: update this to include involved room
+//				room: null
+//			}
+//			return Log.create(newLog)
+//			.then(logResponse => {
+//				res.status(200)
+//				.json({
+//					message: "Attack completed successfully!",
+//					log: logResponse,
+//					attacker: mobs[0],
+//					defender: newMob.value
+//				})
+//			})
+//		})
+//	})
+//	.catch(err => {
+//		console.log(err)
+//		res.status(500).json({err})
+//	})
+//}
+
+//Let's try that again
 actionsController.attack = function(req, res){
-	Promise.all([Mob.findById(req.params.attacker),Mob.findById(req.params.defender)])
-	.then(mobs => {
-		let remainingHealth = mobs[1].attribute.health - mobs[0].attribute.strength;
-		return Mob.update(req.params.defender,"attribute.health",remainingHealth)
-		.then(newMob => {
+	Mob.findById(req.params.attacker)
+	.then(attacker => {
+		return Mob.takeDamage(req.params.defender, attacker.attribute.strength)
+		.then(defender => {
 			let newLog = {
-				content: `${mobs[0].name} attacks ${mobs[1].name} for ${mobs[0].attribute.strength} damage. ** ${mobs[0]._id} =${mobs[0].attribute.strength}=> ${mobs[1]._id} **`,
+				content: "It fuckin' worked and the defender.living is..." + defender.value.attribute.living,
 				timestamp: Date.now(),
 				type: "action",
-				//TODO: update this to include involved room
 				room: null
 			}
 			return Log.create(newLog)
@@ -29,15 +59,15 @@ actionsController.attack = function(req, res){
 				.json({
 					message: "Attack completed successfully!",
 					log: logResponse,
-					attacker: mobs[0],
-					defender: newMob.value
+					attacker,
+					defender: defender.value
 				})
 			})
 		})
 	})
 	.catch(err => {
 		console.log(err)
-		res.status(500).json({err})
+		res.status(500).json({error: err})
 	})
 }
 
