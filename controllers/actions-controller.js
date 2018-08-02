f8a7d2532a67a504b31b2926ec0b99ebdf805b11
Patrick Shannon <pshannon.dev@gmail.com>
2018-08-02 18:13:15 -0400

    Adds some commentary to the attack controller.

--- controllers/actions-controller.js
+++ controllers/actions-controller.js
@@ -13,6 +13,7 @@ const actionsController = {};
 ////Give it logic
 //attacking
 actionsController.attack = function(req, res){
+	//First make sure that both parties are in the same battle...
 	Battle.confirmCompatibleCombatants(req.params.attacker, req.params.defender)
 	.then(response => {
 		if(response.length === 0){
@@ -21,6 +22,7 @@ actionsController.attack = function(req, res){
 			}
 			throw error;
 		}
+		//...Then we grab the attacker(?) and request that the defender takes damage according to specified calculations...
 		Mob.findById(req.params.attacker)
 		.then(attacker => {
 			return Mob.takeDamage(req.params.defender, attacker.attribute.strength)
@@ -29,6 +31,7 @@ actionsController.attack = function(req, res){
 				if (defender.attribute.living){
 					content = `${attacker.name} attacks ${defender.name} for ${attacker.attribute.strength} damage! // ${attacker._id} =${attacker.attribute.strength}=> ${defender._id} //`;
 				}
+				//...But if the defender is already dead then don't do anything and throw and error.
 				else if (defender.attackFailed){
 					console.log('ERROR, CATCH!!!');
 					let error = {};
@@ -38,6 +41,7 @@ actionsController.attack = function(req, res){
 				else {
 					content = `${attacker.name} attacks ${defender.name} for ${attacker.attribute.strength} damage, knocking them to the ground! // ${attacker._id} =x!${attacker.attribute.strength}!x=> ${defender._id} //`;
 				}
+				//Log the action for future reference...
 				let newLog = {
 					content,
 					timestamp: Date.now(),
@@ -57,6 +61,7 @@ actionsController.attack = function(req, res){
 			})
 		})
 	})
+	//General catchall
 	.catch(err => {
 		console.log(err)
 		res.status(500).json({error: err})
